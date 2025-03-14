e3006d9cdadbcfda233c5ea357fc175a744d7221
ayushthelearner <ayushrob4@gmail.com>
2025-03-14 20:22:04 +0530

    adding

--- knee_mri_detection/settings.py
+++ knee_mri_detection/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = "django-insecure-m96y^(&q4p7)wdp5crm%q%58_%)^q#i-(8!fl4bzoqocbu=u+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
